7357f6413ea1bb9849dc72b1fee05218d2658056
MR-PANGLIMAPROJECT <mediasosial232323@gmail.com>
2025-08-19 09:53:03 +0700

    Create Linux.sh

---
+++ tampilan/Linux.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Tampilan Terbaru 2024
+
+########################################
+###   Author = Mr Panglima         #####
+###    Time  = JEMBER ERROR SYSTEM #####
+########################################
+
+# color
+k="\033[1;33m"
+
+clear
+#!/bin/bash
+echo -e $k
+echo "               .--."
+echo "              |o_o | Hai,Saya Pinguin"
+echo "              |:_/ | Saya Distro Linux"
+echo "             //   \\ \\"
+echo "            (|     | )"
+echo "           / \\_   _/ \\"
+echo "           \\___)=(___/"
+echo
+echo "  #  #   ##    ###  #  #  ####  ### "  |lolcat
+echo "  #  #  #  #  #     # #   #     #  # " |lolcat
+echo "  ####  #  #  #     ##    ###   #  # " |lolcat
+echo "  #  #  ####  #     # #   #     #  # " |lolcat
+echo "  #  #  #  #   ###  #  #  ####  ### "  |lolcat
+sleep 2
+echo "=======================================" |lolcat
+echo "[•]         HACKER INDONESIA        [•]" |lolcat
+echo "=======================================" |lolcat
+PS1='\033[01;34m\]┌──\[\033[01;32m\]root\[\033[01;34m\]@\[\033[01;31m\]\h\[\033[00;34m\]\[\033[01;34m\]\w\[\033[00;34m\]\[\033[01;32m\]:\[\033[01;34m\]└╼\[\033[01;31m\]#\[\033[01;32m\]'
